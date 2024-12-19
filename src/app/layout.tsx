074d2d271ed8ece055346507b236944b7abed14d
Vicky <vicky8394@gmail.com>
2024-12-19 20:21:16 +0530

    chore: wip course creation ui page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
+import { ToastProvider } from "@/components/providers/toast-provider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,6 +30,7 @@ export default function RootLayout({
         <body
           className={`${geistSans.variable} ${geistMono.variable} antialiased h-full`}
         >
+          <ToastProvider />
           {children}
         </body>
       </html>
