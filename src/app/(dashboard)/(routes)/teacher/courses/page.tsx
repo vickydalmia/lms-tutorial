08b018c33daf9bf13f106c4f12cd326fdc45e212
Vicky <vicky8394@gmail.com>
2024-12-28 00:14:48 +0530

    chore: added data table for course list

--- src/app/(dashboard)/(routes)/teacher/courses/page.tsx
+++ src/app/(dashboard)/(routes)/teacher/courses/page.tsx
@@ -1,12 +1,25 @@
-import { Button } from "@/components/ui/button";
-import Link from "next/link";
+import { DataTable } from "./_components/data-table";
+import { columns } from "./_components/columns";
+import { db } from "@/lib/db";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 
-const CoursePage = () => {
+async function getData(): Promise<any> {
+  const { userId } = await auth();
+  if (!userId) return redirect("/");
+  const courses = await db.course.findMany({
+    where: {
+      userId,
+    },
+  });
+  return courses;
+}
+
+const CoursePage = async () => {
+  const data = await getData();
   return (
     <div className="p-6">
-      <Link href="/teacher/create">
-        <Button>New Course</Button>
-      </Link>
+      <DataTable columns={columns} data={data} />
     </div>
   );
 };
