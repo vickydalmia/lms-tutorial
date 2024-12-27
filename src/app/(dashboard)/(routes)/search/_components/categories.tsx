637aa8a090ac6a15ea0447a67d1ad08d1afbd9c3
Vicky <vicky8394@gmail.com>
2024-12-28 00:44:45 +0530

    chore: added catgories on top of search page

---
+++ src/app/(dashboard)/(routes)/search/_components/categories.tsx
@@ -0,0 +1,41 @@
+"use client";
+
+import { Category } from "@prisma/client";
+import {
+  FcEngineering,
+  FcFilmReel,
+  FcMultipleDevices,
+  FcMusic,
+  FcOldTimeCamera,
+  FcSalesPerformance,
+  FcSportsMode,
+} from "react-icons/fc";
+import { IconType } from "react-icons/lib";
+import { CategoryItem } from "./category-item";
+
+interface Categoriesprops {
+  items: Category[];
+}
+
+const iconMap: Record<Category["name"], IconType> = {
+  Music: FcMusic,
+  Photography: FcOldTimeCamera,
+  Fitness: FcSportsMode,
+  Accounting: FcSalesPerformance,
+  "Computer Science": FcMultipleDevices,
+  Filming: FcFilmReel,
+  Engineering: FcEngineering,
+};
+export const Categories = ({ items }: Categoriesprops) => {
+  return (
+    <div className="flex items-center gap-x-2 overflow-x-auto pb-2">
+      {items.map((item) => (
+        <CategoryItem
+          label={item.name}
+          icon={iconMap[item.name]}
+          value={item.id}
+        />
+      ))}
+    </div>
+  );
+};
